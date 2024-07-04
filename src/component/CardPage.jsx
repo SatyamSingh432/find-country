3b8d79e52287dcc5d80be3e927d5f0197a285500
Satyam Singh <satyamrajput7239@gmail.com>
2024-07-04 18:30:31 +0530

    change api call method

--- src/component/CardPage.jsx
+++ src/component/CardPage.jsx
@@ -3,25 +3,17 @@ import { useState, useEffect } from "react";
 import FlagCard from "./FlagCard";
 import "./CardPage.css";
 const CardPage = () => {
+  const apiUrl = "https://xcountries-backend.azurewebsites.net/all";
   const [data, setData] = useState([]);
-  useEffect(() => {
-    const apiData = async () => {
-      try {
-        const apiUrl = "https://xcountries-backend.azurewebsites.net/all";
-        const respone = await fetch(apiUrl);
-        if (!respone.ok) {
-          throw new Error("data not found");
-        } else {
-          const data = await respone.json();
-          setData(data);
-        }
-      } catch (error) {
-        console.error(`Error fetching data :${error.message}`);
-      }
-    };
-    apiData();
-  }, []);
 
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error(`Error fetching data :${error.message}`));
+  }, []);
   return (
     <div className="cardPage">
       {data.map((ele) => {
