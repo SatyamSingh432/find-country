25371ebdd87eec74e98e97494e91a6aec8361fbd
Satyam Singh <satyamrajput7239@gmail.com>
2024-07-12 17:26:06 +0530

    fix bug

--- src/component/FlagCard.jsx
+++ src/component/FlagCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./FlagCard.css";
-const FlagCard = (flag, name, alth) => {
+const FlagCard = ({ flag, name, alth }) => {
   return (
     <div className="countryCard">
-      <img src={flag.flag} alt={flag.name} />
-      <h3>{flag.name}</h3>
+      <img src={flag} alt={alth} />
+      <h3>
+        <span>{name}</span>
+      </h3>
     </div>
   );
 };
