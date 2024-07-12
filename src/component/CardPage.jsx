eae3ba18f7b4a409268448468a33de4083357738
Satyam Singh <satyamrajput7239@gmail.com>
2024-07-12 15:05:27 +0530

    intial commit

--- src/component/CardPage.jsx
+++ src/component/CardPage.jsx
@@ -5,28 +5,47 @@ import "./CardPage.css";
 const CardPage = () => {
   const apiUrl = "https://xcountries-backend.azurewebsites.net/all";
   const [data, setData] = useState([]);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data[1].name);
         setData(data);
       })
       .catch((error) => console.error(`Error fetching data:${error.message}`));
   }, []);
+  const handler = (e) => {
+    setName(e.target.value);
+  };
   return (
-    <div className="cardPage">
-      {data.map((ele) => {
-        return (
-          <FlagCard
-            key={`${ele.name}${Math.floor(Math.random() * 90) + 10}`}
-            flag={ele.flag}
-            name={ele.name}
-            alth={ele.alth}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="input-box-container">
+        <input
+          type="text"
+          onChange={handler}
+          className="input-box"
+          placeholder="Enter Country Name"
+        />
+      </div>
+      <div className="cardPage">
+        {data
+          .filter((ele) =>
+            ele.name.toLowerCase().startsWith(name.toLowerCase())
+          )
+          .map((ele) => {
+            return (
+              <FlagCard
+                key={`${ele.name}${Math.floor(Math.random() * 90) + 10}`}
+                flag={ele.flag}
+                name={ele.name}
+                alth={ele.alth}
+              />
+            );
+          })}
+      </div>
+    </>
   );
 };
 export default CardPage;
--- src/component/FlagCard.jsx
+++ src/component/FlagCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./FlagCard.css";
 const FlagCard = (flag, name, alth) => {
   return (
-    <div className="card">
+    <div className="countryCard">
       <img src={flag.flag} alt={flag.name} />
       <h3>{flag.name}</h3>
     </div>
